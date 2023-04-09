2641625df7ac3114c95d76d4feb5ee890591f8b9
Yasin051197 <yasinjamadar0511@gmail.com>
2023-04-09 14:30:21 +0530

    added users css

--- adobe_assignment_app/src/components/AllUsers.jsx
+++ adobe_assignment_app/src/components/AllUsers.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import { Heading, Text } from '@chakra-ui/react'
+import "./AllUsers.css"
 
-
-const getData=()=>{
-    
+const getData=async()=>{
+    return await axios.get("https://adobe-assignment-server.onrender.com/users")
 }
 
 const AllUsers = () => {
     const [allusers,setAllusers]=useState([])
+    useEffect(()=>{
+      getData().then((res)=>setAllusers(res.data))
+    },[])
   return (
-    <div>Allusers</div>
+    <div>
+      <Heading>AllUsers</Heading>
+      <div id="container">
+      {allusers.map((el)=>(
+        <div key={el._id}>
+          <Text>Name:-{el.name}</Text>
+        </div>
+      ))}
+      </div>
+    </div>
   )
 }
 

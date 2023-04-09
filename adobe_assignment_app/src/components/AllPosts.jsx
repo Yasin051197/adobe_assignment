b87d16a170a333f93505bf9712496ea93d241df1
Yasin051197 <yasinjamadar0511@gmail.com>
2023-04-09 16:44:15 +0530

    added css

--- adobe_assignment_app/src/components/AllPosts.jsx
+++ adobe_assignment_app/src/components/AllPosts.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react'
+import "./AllUsers.css"
+
+const getData=async()=>{
+  return await axios.get("https://adobe-assignment-server.onrender.com/posts")
+}
 
 const AllPosts = () => {
+  const [allposts,setAllposts]=useState([])
+    useEffect(()=>{
+      getData().then((res)=>setAllposts(res.data))
+    },[])
+
+    const viewUser=(el)=>{
+      console.log(el)
+    }
+    const editUser=(id)=>{
+      console.log(id)
+    }
+    const deleteUser=(id)=>{
+      console.log(id)
+    }
   return (
-    <div>AllPosts</div>
+    <div>
+      <Heading>AllUsers</Heading>
+      <div id="container">
+      {allposts.map((el)=>(
+        <div id="container_div" key={el._id}>
+          <Text>Content:- {el.content}</Text>
+          <Button onClick={()=>viewUser(el.user_id)} backgroundColor={'gray'} >View</Button>
+          <Button onClick={()=>editUser(el._id)} backgroundColor={'green'}>Edit</Button>
+          <Button onClick={()=>deleteUser(el._id)} backgroundColor={'red'}>Delete</Button>
+        </div>
+      ))}
+      </div>
+    </div>
   )
 }
 
--- adobe_assignment_app/src/components/AllUsers.jsx
+++ adobe_assignment_app/src/components/AllUsers.jsx
@@ -1,6 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
-import { Heading, Text } from '@chakra-ui/react'
+
+import {
+  Box, 
+  Button, 
+  Flex, 
+  Heading, 
+  Text,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverAnchor,
+  Grid,
+} from '@chakra-ui/react'
 import "./AllUsers.css"
 
 const getData=async()=>{
@@ -9,16 +26,41 @@ const getData=async()=>{
 
 const AllUsers = () => {
     const [allusers,setAllusers]=useState([])
+    const [flag,setFlag]=useState(false)
     useEffect(()=>{
       getData().then((res)=>setAllusers(res.data))
     },[])
+
+    const viewUser=(el)=>{
+      
+    }
+    const editUser=(id)=>{
+      console.log(id)
+    }
+    const deleteUser=(id)=>{
+      console.log(id)
+    }
   return (
     <div>
       <Heading>AllUsers</Heading>
       <div id="container">
       {allusers.map((el)=>(
-        <div key={el._id}>
-          <Text>Name:-{el.name}</Text>
+        <div id="container_div" key={el._id}>
+          <Text>User:- {el.name}</Text>
+          <Popover >
+             <PopoverTrigger  >
+              <button onClick={()=>viewUser(el)} style={{backgroundColor:"gray"}}>View</button>
+            </PopoverTrigger>
+            <PopoverContent backgroundColor={'gray'} padding={'20px'} color='white' position={'fixed'}  >
+               <PopoverCloseButton backgroundColor={'gray'}><Button backgroundColor={"black"} color={'white'}>x</Button></PopoverCloseButton>
+                <Text>name- {el.name}</Text>
+                <Text>id- {el._id}</Text>
+                <Text>email- {el.email}</Text>
+                <Text>bio- {el.bio}</Text>
+            </PopoverContent>
+          </Popover>
+          <button onClick={()=>editUser(el._id)} style={{backgroundColor:"green"}}>Edit</button>
+          <button onClick={()=>deleteUser(el._id)} style={{backgroundColor:"red"}}>Delete</button>
         </div>
       ))}
       </div>
